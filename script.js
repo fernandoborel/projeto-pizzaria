e6b0b1b09ea23fb63939c34f7f04f9301c87779e
Fernando Borel <fernandomenezesborel@gmail.com>
2021-08-14 17:03:50 -0300

    Botões cancelar e voltar ativos

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let modalQt = 1;
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
-
+// listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     pizzaItem.setAttribute('data-key', index);
@@ -12,12 +14,24 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
         
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;        
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
         
+        c('.pizzaInfo--size.selected').classList.remove('selected');
         
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        
+        });
+        
+        c('.pizzaInfo--qt').innerHTML = modalQt;
         
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
@@ -28,3 +42,16 @@ pizzaJson.map((item, index)=>{
     
     c('.pizza-area').append( pizzaItem );
 });
+
+// eventos do modal
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+
+};
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+})
