00d4fca72d74ad487b3445b408f9eccd9d6435e6
Fernando Borel <fernandomenezesborel@gmail.com>
2021-08-16 23:09:10 -0300

    Footer adicionado

--- script.js
+++ script.js
@@ -116,8 +116,14 @@ function updateCart(){
         c('aside').classList.add('show');
         c('.cart').innerHTML = ' ';
         
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+        
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+            
             let cartItem = c('.models .cart--item').cloneNode(true);
 
             //preenchendo o carrinho
@@ -140,12 +146,33 @@ function updateCart(){
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
-            
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
             
             c('.cart').append(cartItem);
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     
     } else {
         c('aside').classList.remove('show');
     }
 };
+
+
