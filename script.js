b26943bc09a090f154aee54e5b976f4b7a9d317a
Fernando Borel <fernandomenezesborel@gmail.com>
2021-08-16 21:29:09 -0300

    imagens no carrinho

--- script.js
+++ script.js
@@ -114,10 +114,35 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 function updateCart(){
     if(cart.length > 0) {
         c('aside').classList.add('show');
+        c('.cart').innerHTML = ' ';
+        
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            let cartItem = c('.models .cart--item').cloneNode(true);
 
-            console.log(pizzaItem);
+            //preenchendo o carrinho
+            
+            let pizzaSizeName;
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+            
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+            
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            
+            
+            c('.cart').append(cartItem);
         }
     
     } else {
